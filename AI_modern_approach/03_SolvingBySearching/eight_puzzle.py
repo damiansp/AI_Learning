67a8b42809d0e22412cc40b273deddfe80995557
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-05-20 15:05:55 -0400

    simplifies design

--- AI_modern_approach/03_SolvingBySearching/eight_puzzle.py
+++ AI_modern_approach/03_SolvingBySearching/eight_puzzle.py
@@ -5,66 +5,65 @@ UP    = [ 1,  0]
 DOWN  = [-1,  0]
 LEFT  = [ 0,  1]
 RIGHT = [ 0, -1]
-GOAL = [[1, 2, 3],
-        [4, 0, 5],
-        [6, 7, 8]]
+GOAL = np.array([[1, 2, 3],
+                 [4, 0, 5],
+                 [6, 7, 8]])
 
 
-class Board:
-    def __init__(self, state):
-        self.state = state
-        self.gap = self._get_gap()
 
-    def __str__(self):
-        return ('[%s,\n %s\n %s]\n'
-                % (self.state[0], self.state[1], self.state[2]))
+def make_random_board(dim=3):
+    tiles = list(range(dim * dim))
+    np.random.shuffle(tiles)
+    return [tiles[(i*dim):(i*dim + dim)] for i in range(dim)]
 
-    def _get_gap(self):
-        for i in range(3):
-            for j in range(3):
-                if self.state[i][j] == 0:
-                    return [i, j]
-                
 
-    def move(self, direction):
-        assert direction in [UP, DOWN, LEFT, RIGHT]
-        try:
-            gap_position = self._get_gap()
-            swap_position = [gap_position[0] + direction[0],
-                             gap_position[1] + direction[1]]
-            swap_value = self.state[swap_position[0]][swap_position[1]]
-            self.state[gap_position[0]][gap_position[1]] = swap_value
-            self.state[swap_position[0]][swap_position[1]] = 0
-        except KeyError:
-            print('Cannot move that direction')
+def get_gap(board):
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == 0:
+                return [i, j]
 
-class Game:
-    def __init__(self):
-        self.goal = Board(GOAL)
-        self.initial_state = self._make_random_board()
-        self.explored_states = []
-        self.unexplored_states = []
+            
+def print_board(board):
+    print('[%s,\n %s\n %s]' % (board[0], board[1], board[2]))
+    
+            
+def move(board, direction):
+    assert direction in [UP, DOWN, LEFT, RIGHT]
+    try:
+        gap_position = get_gap(board)
+        swap_position = [gap_position[0] + direction[0],
+                         gap_position[1] + direction[1]]
+        swap_value = board[swap_position[0]][swap_position[1]]
+        board[gap_position[0]][gap_position[1]] = swap_value
+        board[swap_position[0]][swap_position[1]] = 0
+    except IndexError:
+        print('Cannot move that direction')
+    return board
 
-    def _make_random_board(self):
-        dim = len(self.goal.state)
-        tiles = list(range(dim * dim))
-        np.random.shuffle(tiles)
-        return [tiles[(i*dim):(i*dim + dim)] for i in range(dim)]
+        
+def get_all_successive_states(board):
+    successive_states = []
+    for direction in [UP, DOWN, LEFT, RIGHT]:
+        temp_board = np.copy(board)
+        new_state = move(temp_board, direction)
+        successive_states.append(new_state)
+    return successive_states
 
-    def get_all_successive_states(self):
-        pass
 
-game = Game()
-print(game.initial_state)
+
             
 
-
 def main():
-    board = Board(GOAL)
-    print(board)
-    board.move(UP)
-    print(board)
-    board.move(LEFT)
-    print(board)
+    board = make_random_board()
+    print_board(board)
+    print('\nTesting all moves...')
+    for direction in [UP, DOWN, LEFT, RIGHT]:
+        board = move(board, direction)
+        print_board(board)
+    print('\nTesting successive states...')
+    successive_states = get_all_successive_states(board)
+    for state in successive_states:
+        print_board(state)
     
 main()
